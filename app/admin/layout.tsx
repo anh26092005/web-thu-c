710c5840287bfceb241bf484e5d5e1ebd23c697f
Anh <anhdlcwk@gmail.com>
2025-07-08 21:54:26 +0700

    1

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -14,20 +14,20 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 flex flex-col">
       {/* Header */}
       <AdminHeader />
       
-      <div className="flex">
+      <div className="flex flex-1">
         {/* Sidebar */}
         <AdminSidebar />
         
         {/* Main content */}
-        <div className=" p-6 m-0">
-          <div className="w-full mx-auto">
+        <main className="flex-1 p-6 overflow-auto">
+          <div className="w-full mx-auto max-w-7xl">
             {children}
           </div>
-        </div>
+        </main>
       </div>
     </div>
   );
